11376d55f6dd07e5c45ec4db510bd8aa12a511f4
Anes Hodza <hodzaane@students.zhaw.ch>
2025-04-24 03:05:13 +0200

    Add back accidental removal of output write

--- src/bresenham.c
+++ src/bresenham.c
@@ -19,6 +19,7 @@ int bresenham(Point p0, Point p1, Point* output) {
     if (y0 == 0 && x0 != 0) {
       // TODO: Fix this error
     }
+    output[i++] = (Point){x0, y0};
     if (x0 == x1 && y0 == y1) break;
 
     int e2 = 2 * err;
